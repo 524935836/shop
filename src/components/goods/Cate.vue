ebbdb22ac8d6002217ef4b0879ab0fbdb90de34b
zxk <524935836@qq.com>
2021-12-04 17:35:42 +0800

    完成了商品分类功能

---
+++ src/components/goods/Cate.vue
@@ -0,0 +1,241 @@
+<template>
+  <div>
+    <!-- 面包屑区域 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>商品分类</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片内容 -->
+    <el-card>
+      <!-- 添加分类 -->
+      <el-row>
+        <el-col>
+          <el-button type="primary" @click="showAddCateDialog">添加分类</el-button>
+        </el-col>
+      </el-row>
+      <!-- 表格 -->
+      <tree-table
+        :data="cateList"
+        :columns="columns"
+        show-index
+        index-text="#"
+        border
+        :show-row-hover="false"
+        :expand-type="false"
+        :selection-type="false"
+        class="treeTable"
+      >
+        <!-- 是否有效 -->
+        <template slot="isOk" scope="{ row }">
+          <i
+            class="el-icon-success"
+            v-if="!row.cat_deleted"
+            style="color: lightgreen"
+          ></i>
+          <i class="el-icon-error" v-else style="color: red"></i>
+        </template>
+        <!-- 排序 -->
+        <template slot="order" scope="{ row }">
+          <el-tag v-if="row.cat_level === 0" size="mini">一级</el-tag>
+          <el-tag type="success" v-else-if="row.cat_level === 1" size="mini">二级</el-tag>
+          <el-tag type="warning" v-else size="mini">三级</el-tag>
+        </template>
+        <!-- 操作 -->
+        <template slot="opt">
+          <el-button type="primary" icon="el-icon-edit" size="mini">编辑</el-button>
+          <el-button type="danger" icon="el-icon-delete" size="mini">删除</el-button>
+        </template>
+      </tree-table>
+      <!-- 分页区域 -->
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="queryInfo.pagenum"
+        :page-sizes="[3, 5, 10, 15]"
+        :page-size="queryInfo.pagesize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total"
+      >
+      </el-pagination>
+    </el-card>
+    <!-- 添加分类的对话框 -->
+    <el-dialog
+      title="添加分类"
+      :visible.sync="addCateDialogVisible"
+      width="50%"
+      @close="addCateDialogClosed"
+    >
+      <!-- 表单验证 -->
+      <el-form
+        :model="addCateForm"
+        :rules="addCateFormRules"
+        ref="addCateFormRef"
+        label-width="100px"
+      >
+        <el-form-item label="分类名称" prop="cat_name">
+          <el-input v-model="addCateForm.cat_name"></el-input>
+        </el-form-item>
+        <el-form-item label="父级分类">
+          <!-- 父级分类级联选择器 -->
+          <el-cascader
+            v-model="selectKey"
+            :options="parentCateList"
+            :props="cascaderProps"
+            @change="parentCateChanged"
+            clearable
+            class="el-cascader-menu"
+          >
+          </el-cascader>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="addCateDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addCate">确 定</el-button>
+      </span>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Cate',
+  data() {
+    return {
+      // 请求参数
+      queryInfo: {
+        type: 3,
+        pagenum: 1,
+        pagesize: 5
+      },
+      // 商品分类列表
+      cateList: [],
+      // 总数量
+      total: 0,
+      // 表格单列的规则
+      columns: [
+        {
+          label: '分类名称',
+          prop: 'cat_name'
+        },
+        {
+          label: '是否有效',
+          type: 'template',
+          template: 'isOk'
+        },
+        {
+          label: '排序',
+          type: 'template',
+          template: 'order'
+        },
+        {
+          label: '操作',
+          type: 'template',
+          template: 'opt'
+        }
+      ],
+      // 添加分类对话框的开关
+      addCateDialogVisible: false,
+      // 添加分类的数据
+      addCateForm: {
+        cat_pid: 0,
+        cat_name: '',
+        cat_level: 0
+      },
+      // 添加分类的规则
+      addCateFormRules: {
+        cat_name: [{ required: true, message: '请输入分类名称', trigger: 'blur' }]
+      },
+      // 父级商品分类列表
+      parentCateList: [],
+      // 父级商品分类级联选择器配置项
+      cascaderProps: {
+        expandTrigger: 'hover',
+        // v-model绑定value，作为selectKey数组中的id
+        value: 'cat_id',
+        label: 'cat_name',
+        children: 'children',
+        checkStrictly: true
+      },
+      // 选中的父级分类的id数组
+      selectKey: []
+    }
+  },
+  created() {
+    this.getCateList()
+  },
+  methods: {
+    // 获取商品分类
+    async getCateList() {
+      const { data: res } = await this.$http.get('categories', { params: this.queryInfo })
+      if (res.meta.status !== 200) return this.$message.error('获取商品分类失败')
+      this.cateList = res.data.result
+      // 总数量
+      this.total = res.data.total
+    },
+    // 单页项目个数发生改变的回调
+    handleSizeChange(newSize) {
+      this.queryInfo.pagesize = newSize
+      this.getCateList()
+    },
+    // 页码发生变化的回调
+    handleCurrentChange(newNum) {
+      this.queryInfo.pagenum = newNum
+      this.getCateList()
+    },
+    // 点击显示添加分类对话框
+    showAddCateDialog() {
+      this.getParentCateList()
+      this.addCateDialogVisible = true
+    },
+    //  获取父级的分类
+    async getParentCateList() {
+      const { data: res } = await this.$http.get('categories', {
+        params: {
+          type: 2
+        }
+      })
+      if (res.meta.status !== 200) return this.$message.error('获取父级商品分类失败')
+      this.parentCateList = res.data
+    },
+    // 次联选择器子节点发生变化时触发
+    parentCateChanged() {
+      if (this.selectKey.length > 0) {
+        // 更新父级id的值
+        this.addCateForm.cat_pid = this.selectKey[this.selectKey.length - 1]
+        // 更新分类层级
+        this.addCateForm.cat_level = this.selectKey.length
+      } else {
+        this.addCateForm.cat_pid = 0
+        this.addCateForm.cat_level = 0
+      }
+    },
+    addCate() {
+      this.$refs.addCateFormRef.validate(async (valid) => {
+        if (!valid) return
+        const { data: res } = await this.$http.post('categories', this.addCateForm)
+        if (res.meta.status !== 201) return this.$message.error('添加分类失败')
+        this.getCateList()
+        this.addCateDialogVisible = false
+        this.$message.success('添加分类成功')
+      })
+    },
+    // 关闭添加分类对话框回调
+    addCateDialogClosed() {
+      this.$refs.addCateFormRef.resetFields()
+      this.selectKey = []
+      this.addCateForm.cat_pid = 0
+      this.addCateForm.cat_level = 0
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.treeTable {
+  margin-top: 15px;
+}
+.el-cascader {
+  width: 100%;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,11 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
-// 导入全局样式
-import './assets/css/global.css'
-
 // 导入字体图标
 import './assets/fonts/iconfont.css'
+// 导入全局样式
+import './assets/css/global.css'
+// 引入treeTable插件
+import TreeTable from 'vue-table-with-tree-grid'
 
 import axios from 'axios'
 // 配置请求路径
@@ -19,6 +20,8 @@ axios.interceptors.request.use(config => {
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
+
+Vue.component('tree-table', TreeTable)
 
 new Vue({
   router,
